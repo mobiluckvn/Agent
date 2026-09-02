035ee505b4bae30a59a025fb9b356fbf53054abd
Embedded AIDD Agent <eaa@localhost>
2026-09-02 08:46:25 +0700

    drv_i2c: mã sinh bởi quy trình AIDD
    prompt-hash: sha256:ab01dc620f0758219229756e9237e29b410ef6f9b46ebe880f9b3d5347fd4c60
    model: gemini-3.1-pro-preview
    constraints-version: sha256:a3aef55c34e89780f29f3d4a3503e90ea3407e8505027221d19566f813932b16
    chunk-ids: ds-021,ds-022,ds-023
    tokens-in: 3653
    tokens-out: 4074

--- src/drv_i2c.c
+++ src/drv_i2c.c
@@ -3,120 +3,157 @@
 #include <avr/interrupt.h>
 #include <stddef.h>
 
-static volatile uint8_t i2c_status = I2C_STATUS_IDLE;
-static volatile uint8_t i2c_addr;
-static uint8_t * volatile i2c_buf;
-static volatile uint8_t i2c_len;
-static volatile uint8_t i2c_idx;
+#define I2C_TIMEOUT_TICKS 5
+
+volatile i2c_status_t i2c_status = I2C_IDLE;
+volatile uint8_t i2c_timeout_counter = 0;
+
+volatile uint8_t i2c_addr;
+volatile const uint8_t *i2c_tx_data;
+volatile uint8_t *i2c_rx_data;
+volatile uint8_t i2c_len;
+volatile uint8_t i2c_idx;
+volatile bool i2c_is_read;
 
 void i2c_init(void) {
     // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
     TWSR = 0x00;
     TWBR = 12;
-    TWCR = (1 << 2); // TWEN
+    
+    uint8_t sreg = SREG;
+    cli();
+    if (TWCR & (1 << TWEN)) {
+        TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+    }
+    i2c_status = I2C_IDLE;
+    i2c_timeout_counter = 0;
+    i2c_tx_data = NULL;
+    i2c_rx_data = NULL;
+    i2c_len = 0;
+    i2c_idx = 0;
+    
+    TWCR = (1 << TWEN) | (1 << TWIE);
+    SREG = sreg;
 }
 
 bool i2c_write_async(uint8_t addr, const uint8_t *data, uint8_t len) {
-    if (i2c_status == I2C_STATUS_BUSY) {
+    uint8_t sreg = SREG;
+    cli();
+    if (i2c_status == I2C_BUSY) {
+        SREG = sreg;
         return false;
     }
-    if (len == 0 || data == NULL) {
-        return false;
-    }
-
+    i2c_status = I2C_BUSY;
     i2c_addr = addr << 1;
-    i2c_buf = (uint8_t *)data;
+    i2c_tx_data = data;
     i2c_len = len;
     i2c_idx = 0;
-    i2c_status = I2C_STATUS_BUSY;
-
+    i2c_is_read = false;
+    i2c_timeout_counter = 0;
+    
     // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-    TWCR = (1 << 7) | (1 << 5) | (1 << 2) | (1 << 0); // TWINT | TWSTA | TWEN | TWIE
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN) | (1 << TWIE);
+    SREG = sreg;
     return true;
 }
 
 bool i2c_read_async(uint8_t addr, uint8_t *data, uint8_t len) {
-    if (i2c_status == I2C_STATUS_BUSY) {
+    uint8_t sreg = SREG;
+    cli();
+    if (i2c_status == I2C_BUSY) {
+        SREG = sreg;
         return false;
     }
-    if (len == 0 || data == NULL) {
-        return false;
-    }
-
+    i2c_status = I2C_BUSY;
     i2c_addr = (addr << 1) | 1;
-    i2c_buf = data;
+    i2c_rx_data = data;
     i2c_len = len;
     i2c_idx = 0;
-    i2c_status = I2C_STATUS_BUSY;
-
+    i2c_is_read = true;
+    i2c_timeout_counter = 0;
+    
     // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-    TWCR = (1 << 7) | (1 << 5) | (1 << 2) | (1 << 0); // TWINT | TWSTA | TWEN | TWIE
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN) | (1 << TWIE);
+    SREG = sreg;
     return true;
 }
 
 i2c_status_t i2c_get_status(void) {
-    return (i2c_status_t)i2c_status;
+    i2c_status_t st;
+    uint8_t sreg = SREG;
+    cli();
+    st = i2c_status;
+    SREG = sreg;
+    return st;
+}
+
+void i2c_tick(void) {
+    uint8_t sreg = SREG;
+    cli();
+    if (i2c_status == I2C_BUSY) {
+        i2c_timeout_counter++;
+        if (i2c_timeout_counter > I2C_TIMEOUT_TICKS) {
+            // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
+            TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO) | (1 << TWIE);
+            i2c_status = I2C_ERROR;
+        }
+    }
+    SREG = sreg;
 }
 
 ISR(TWI_vect) {
     // ref: ds-022, ATmega328P datasheet rev. DS40002061B, tr.228-232
     uint8_t status = TWSR & 0xF8;
-
+    
     switch (status) {
-        case 0x08: // START
-        case 0x10: // Repeated START
+        case 0x08: // START transmitted
+        case 0x10: // Repeated START transmitted
             TWDR = i2c_addr;
-            // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-            TWCR = (1 << 7) | (1 << 2) | (1 << 0); // TWINT | TWEN | TWIE
+            TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE);
             break;
-
-        case 0x18: // SLA+W, ACK
-        case 0x28: // Data sent, ACK
+            
+        case 0x18: // SLA+W transmitted, ACK received
+        case 0x28: // Data transmitted, ACK received
             if (i2c_idx < i2c_len) {
-                TWDR = i2c_buf[i2c_idx++];
-                // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-                TWCR = (1 << 7) | (1 << 2) | (1 << 0); // TWINT | TWEN | TWIE
+                TWDR = i2c_tx_data[i2c_idx++];
+                TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE);
             } else {
-                // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-                TWCR = (1 << 7) | (1 << 4) | (1 << 2); // TWINT | TWSTO | TWEN
-                i2c_status = I2C_STATUS_SUCCESS;
+                TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTO);
+                i2c_status = I2C_SUCCESS;
             }
             break;
-
-        case 0x40: // SLA+R, ACK
-            if (i2c_len == 1) {
-                // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-                TWCR = (1 << 7) | (1 << 2) | (1 << 0); // TWINT | TWEN | TWIE
+            
+        case 0x40: // SLA+R transmitted, ACK received
+            if (i2c_len > 1) {
+                TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWEA);
             } else {
-                // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-                TWCR = (1 << 7) | (1 << 6) | (1 << 2) | (1 << 0); // TWINT | TWEA | TWEN | TWIE
+                TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE);
             }
             break;
-
-        case 0x50: // Data received, ACK
-            i2c_buf[i2c_idx++] = TWDR;
-            if (i2c_idx == i2c_len - 1) {
-                // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-                TWCR = (1 << 7) | (1 << 2) | (1 << 0); // TWINT | TWEN | TWIE
+            
+        case 0x50: // Data received, ACK returned
+            i2c_rx_data[i2c_idx++] = TWDR;
+            if (i2c_idx < i2c_len - 1) {
+                TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWEA);
             } else {
-                // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-                TWCR = (1 << 7) | (1 << 6) | (1 << 2) | (1 << 0); // TWINT | TWEA | TWEN | TWIE
+                TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE);
             }
             break;
-
-        case 0x58: // Data received, NACK
-            i2c_buf[i2c_idx++] = TWDR;
-            // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-            TWCR = (1 << 7) | (1 << 4) | (1 << 2); // TWINT | TWSTO | TWEN
-            i2c_status = I2C_STATUS_SUCCESS;
+            
+        case 0x58: // Data received, NACK returned
+            i2c_rx_data[i2c_idx++] = TWDR;
+            TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTO);
+            i2c_status = I2C_SUCCESS;
             break;
-
-        case 0x20: // SLA+W, NACK
+            
+        case 0x20: // SLA+W transmitted, NACK received
+        case 0x30: // Data transmitted, NACK received
+        case 0x48: // SLA+R transmitted, NACK received
         case 0x38: // Arbitration lost
         default:
-            // ref: ds-021, ATmega328P datasheet rev. DS40002061B, tr.222-224
-            TWCR = (1 << 7) | (1 << 4) | (1 << 2); // TWINT | TWSTO | TWEN
-            i2c_status = I2C_STATUS_ERROR;
+            TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTO);
+            i2c_status = I2C_ERROR;
             break;
     }
+    i2c_timeout_counter = 0;
 }
--- src/drv_i2c.h
+++ src/drv_i2c.h
@@ -5,15 +5,16 @@
 #include <stdbool.h>
 
 typedef enum {
-    I2C_STATUS_IDLE = 0,
-    I2C_STATUS_BUSY,
-    I2C_STATUS_SUCCESS,
-    I2C_STATUS_ERROR
+    I2C_IDLE = 0,
+    I2C_BUSY = 1,
+    I2C_SUCCESS = 2,
+    I2C_ERROR = 3
 } i2c_status_t;
 
 void i2c_init(void);
 bool i2c_write_async(uint8_t addr, const uint8_t *data, uint8_t len);
 bool i2c_read_async(uint8_t addr, uint8_t *data, uint8_t len);
 i2c_status_t i2c_get_status(void);
+void i2c_tick(void);
 
 #endif // DRV_I2C_H
